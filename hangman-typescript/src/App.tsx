d5acbf149ed8f7d3a8613f9d26c0d83bd99cce5e
Dranas786 <dranas2002@gmail.com>
2025-01-10 21:14:19 -0700

    msg

--- hangman-typescript/src/App.tsx
+++ hangman-typescript/src/App.tsx
@@ -4,10 +4,12 @@ import { HangmanDrawing } from "./HangmanDrawing";
 import { HangmanWord } from "./HangmanWord";
 import { Keyboard } from "./Keyboard";
 
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)];
-  });
+  const [wordToGuess, setWordToGuess] = useState(getWord());
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   const incorrectLetters = guessedLetters.filter(
@@ -18,6 +20,7 @@ function App() {
   const isWinner = wordToGuess
     .split("")
     .every((letter) => guessedLetters.includes(letter));
+
   const addGuessedLetter = useCallback(
     (letter: string) => {
       if (guessedLetters.includes(letter) || isWinner || isLoser) return;
@@ -34,6 +37,22 @@ function App() {
 
       e.preventDefault();
       addGuessedLetter(key);
+    };
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (key != "Enter") return;
+
+      e.preventDefault();
+      setWordToGuess(getWord());
+      setGuessedLetters([]);
     };
     document.addEventListener("keypress", handler);
 
